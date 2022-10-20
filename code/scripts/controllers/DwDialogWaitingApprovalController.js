5c246a0a83f7e136a08aed64aeccaf846e43e3a7
Teodor Lupu <luputeodor2@gmail.com>
2022-10-20 11:54:06 +0300

    PharmaLedger-IMI/epi-workspace#906 added toast messages regarding the inserted recovery code

--- code/scripts/controllers/DwDialogWaitingApprovalController.js
+++ code/scripts/controllers/DwDialogWaitingApprovalController.js
@@ -1,9 +1,12 @@
+import {DwUI} from "./DwController.js";
+
 const {DwController} = WebCardinal.controllers;
 
 export default class DwDialogWaitingApprovalController extends DwController {
     constructor(...props) {
         super(...props);
 
+        this.showToast = new DwUI().showToast;
         this.tagClickListeners();
     }
 
@@ -15,30 +18,71 @@ export default class DwDialogWaitingApprovalController extends DwController {
         });
         this.onTagClick("continue", async () => {
             if (document.getElementById("add-member-input").value === "") {
+                await this.showToast(`Please insert a recovery code.`, {
+                    duration: 3000,
+                });
                 return;
             }
-            this.setSharedEnclaveKeySSI().then().catch(err => {
-                console.log(err);
+            this.setSharedEnclaveKeySSI().then(
+                () => {
+                    this.getSharedEnclaveKeySSI().then(
+                        () => {
+                            this.ui.enableMenu();
+                            this.navigateToPageTag("quick-actions");
+                        }
+                    ).catch(async err => {
+                        this.model.notAuthorized = true;
+                        console.log("sharedEnclave doesn't have a defined KeySSI. " + err);
+                        await this.showToast(`Recovery code is not valid.`, {
+                            duration: 3000,
+                        });
+                    })
+                }
+            ).catch(async err => {
+                await this.showToast(`Recovery code is not valid.`, {
+                    duration: 3000,
+                });
             });
-
-            this.ui.enableMenu();
-            this.navigateToPageTag("quick-actions");
         });
     }
 
     async setSharedEnclaveKeySSI() {
+        return new Promise((resolve, reject) => {
+            const openDSU = require("opendsu");
+            const scAPI = openDSU.loadAPI("sc");
+            const keySSI = openDSU.loadAPI("keyssi");
+            const enclaveAPI = openDSU.loadAPI("enclave");
+            const recoveryCode = document.getElementById("add-member-input").value;
+
+            try {
+                keySSI.parse(recoveryCode); // parse and check if the recoveryCode has the right format for a sharedEnclaveKeySSI
+            } catch (err) {
+                debugger
+                return reject(err);
+            }
+
+            const sharedEnclave = enclaveAPI.initialiseWalletDBEnclave(recoveryCode);
+            sharedEnclave.on("initialised", async () => {
+                try {
+                    await $$.promisify(scAPI.setSharedEnclave)(sharedEnclave);
+                    resolve();
+                } catch (e) {
+                    reject("Failed to set shared enclave.");
+                }
+            });
+        });
+    }
+
+    async getSharedEnclaveKeySSI() {
         const openDSU = require("opendsu");
         const scAPI = openDSU.loadAPI("sc");
-        const enclaveAPI = openDSU.loadAPI("enclave");
-        const recoveryCode = document.getElementById("add-member-input").value;
 
-        const sharedEnclave = enclaveAPI.initialiseWalletDBEnclave(recoveryCode);
-        sharedEnclave.on("initialised", async () => {
-            try {
-                await $$.promisify(scAPI.setSharedEnclave)(sharedEnclave);
-            } catch (e) {
-                throw ("Failed to set shared enclave " + e);
-            }
-        });
+        let sharedEnclave;
+        try {
+            sharedEnclave = await $$.promisify(scAPI.getSharedEnclave)();
+        } catch (e) {
+            console.log("Failed to get shared enclave " + e);
+        }
+        return sharedEnclave.getKeySSIAsync();
     }
 }
