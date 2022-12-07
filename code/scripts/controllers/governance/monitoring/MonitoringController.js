5c043c188fe0d77ea7d9538ef32758f4e4af98ad
paiuscatalin <p.cata95@gmail.com>
2022-12-07 17:53:27 +0200

    implemented monitoring page

--- code/scripts/controllers/governance/monitoring/MonitoringController.js
+++ code/scripts/controllers/governance/monitoring/MonitoringController.js
@@ -1,3 +1,5 @@
+import constants from '../../../constants.js';
+
 const { DwController } = WebCardinal.controllers;
 
 class MonitoringUI extends DwController {
@@ -6,7 +8,11 @@ class MonitoringUI extends DwController {
   }
 
   getInitialViewModel() {
-    return {};
+    return {
+      url: '',
+      displayUrl: '',
+      hasUrl: false
+    };
   }
 }
 
@@ -17,8 +23,76 @@ class MonitoringController extends DwController {
     this.ui.page = new MonitoringUI(...props);
     this.model = this.ui.page.getInitialViewModel();
 
-    this.selectedOrganization = this.model.toObject('selectedOrganization');
-    console.log("organization data: ", this.selectedOrganization);
+    this.init();
+  }
+
+  init() {
+    this.attachViewListeners();
+    this.updateModel();
+  }
+
+  attachViewListeners() {
+    this.attachInputUrlHandler();
+    this.attachPasteHandler();
+    this.attachAddUrlHandler();
+    this.attachClearUrlHandler();
+  }
+
+  attachPasteHandler() {
+    this.onTagClick('url.paste', async (model, target) => {
+      try {
+        const result = await navigator.permissions.query({
+          name: 'clipboard-read'
+        });
+        if (result.state === 'granted' || result.state === 'prompt') {
+          const url = await navigator.clipboard.readText();
+          target.parentElement.value = url;
+          return { url };
+        }
+        throw Error('Coping from clipboard is not possible!');
+      } catch (err) {
+        target.remove();
+        console.log(err);
+        return '';
+      }
+    });
+  }
+
+  attachAddUrlHandler() {
+    this.onTagClick('url.add', async () => {
+      const selectedOrganization = this.model.toObject('selectedOrganization');
+      selectedOrganization.monitoringUrl = this.model.url;
+      await this.sharedStorageService.updateRecordAsync(constants.TABLES.GOVERNANCE_ORGANIZATIONS, selectedOrganization.pk, selectedOrganization);
+
+      this.model.displayUrl = this.model.url;
+      this.model.hasUrl = this.model.displayUrl.trim().length > 0;
+    });
+  }
+
+  attachClearUrlHandler() {
+    this.onTagClick('url.remove', async () => {
+      const selectedOrganization = this.model.toObject('selectedOrganization');
+      selectedOrganization.monitoringUrl = '';
+      await this.sharedStorageService.updateRecordAsync(constants.TABLES.GOVERNANCE_ORGANIZATIONS, selectedOrganization.pk, selectedOrganization);
+
+      this.model.displayUrl = '';
+      this.model.hasUrl = false;
+    });
+  }
+
+  updateModel() {
+    const selectedOrganization = this.model.toObject('selectedOrganization');
+    this.model.url = selectedOrganization.monitoringUrl || '';
+    this.model.hasUrl = this.model.url.trim().length > 0;
+  }
+
+  attachInputUrlHandler() {
+    const htmlEl = document.querySelector('#url-input');
+    const handler = (event) => {
+      this.model.url = event.target.value;
+    };
+    htmlEl.addEventListener('sl-change', handler);
+    htmlEl.addEventListener('sl-input', handler);
   }
 }
 
